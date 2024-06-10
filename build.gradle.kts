7f8da04a6f6422f5fdc81f14fba152487715f74f
Anston Sorensen <ansorensen1118@gmail.com>
2024-06-10 15:20:53 -0500

    Change build system to Gradle
    Signed-off-by: Anston Sorensen <ansorensen1118@gmail.com>

---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+plugins {
+    id("java")
+}
+
+group = "com.acclash"
+version = "0.4"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("net.dv8tion:JDA:5.0.0-alpha.12")
+}
